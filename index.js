06f44e56e3502e644f9a79269fb7cbf405a1612a
Sihab-uddin-1212 <shihabsk07742@gmail.com>
2025-08-27 21:04:44 +0600

    second commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ for (let btn of heartButtons) {
     document.getElementById('heart-count').innerText = count;
     
     console.log( count);
-  });
+  }); 
 }
 
 // ---------------- ******for copy count************------------
@@ -25,5 +25,42 @@ for (let copy  of copyes) {
     document.getElementById('copy-btn').innerText = count;
 
     console.log( count);
+  });
+}
+ 
+// -----------------*********** for call buttom ***********----------
+ 
+
+ 
+const callButtons = document.getElementsByClassName("call-number");
+
+ 
+const historyContainer = document.getElementsByClassName("histori-container")[0];
+ 
+for (let callButton of callButtons) {
+  callButton.addEventListener("click", function () {
+     
+    let card = callButton.closest(".bg-white");
+ 
+    let serviceName = card.getElementsByTagName("h2")[0].innerText;
+ 
+    let serviceNumber = card.getElementsByTagName("h2")[1].innerText;
+ 
+    let now = new Date().toLocaleTimeString();
+    
+    let div = document.createElement("div");
+    div.className =
+      "history-containar flex justify-between items-center m-3 p-3 bg-[#D9D9D9] rounded-lg";
+    div.innerHTML = `
+      <div class="name-container">
+        <h2 class="font-bold">${serviceName}</h2>
+        <p>${serviceNumber}</p>
+      </div>
+      <div class="time">
+        <p>${now}</p>
+      </div>
+    `;
+ 
+    historyContainer.appendChild(div);
   });
 }
